127bc526119e22b5fb7eebc7430eee4871d37a9d
dldmstn512 <lessoon512@naver.com>
2025-06-23 00:29:44 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20240897/mysql_music/MysqlMusicApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20240897.mysql_music
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlMusicApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlMusicApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20240897/mysql_music/MysqlMusicApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20240897.mysql_music
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlMusicApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
